34f800bf868d55ad6613031a9afa41933db294a5
Shestakov Evgeny <lester_g@inbox.ru>
2020-07-29 19:37:42 +0300

    1.5. Поля объекта [#336398]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,29 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private  String food;
+    private String name;
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        Cat gav = new Cat();
+        Cat black = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("cutlet");
+        black.giveNick("Black");
+        black.eat("fish");
+        gav.show();
+        black.show();
     }
+
+    void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void show() {
+        System.out.println(this.name + " eats a " + this.food);
+    }
+
 }
