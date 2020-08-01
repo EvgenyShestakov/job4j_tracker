6f2065c6abcfdf62a64cc6c8c36aa8aed31f7bda
Shestakov Evgeny <lester_g@inbox.ru>
2020-08-01 09:25:10 +0300

    6. Сдача в кофе машине [#336392]

---
+++ src/main/java/ru/job4j/sort/Machine.java
@@ -0,0 +1,23 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Machine {
+    private final int[] coins = {10, 5, 2, 1};
+
+    public int[] change(int money, int price) {
+        int[] rsl = new int[100];
+        int size = 0;
+        int shortchange = money - price;
+        for (int i = 0; i < coins.length; i++) {
+            while (shortchange - coins[i] >= 0) {
+                shortchange = shortchange - coins[i];
+                rsl[size++] = coins[i];
+                if (shortchange == 0) {
+                    break;
+                }
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}
