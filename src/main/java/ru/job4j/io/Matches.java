646e4960b51ba085e4f3951e6f12a402666a03be
Shestakov Evgeny <lester_g@inbox.ru>
2020-08-07 18:32:01 +0300

    2. Scanner и чтение числа из консоли. [#336363]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -5,33 +5,24 @@ import java.util.Scanner;
 public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean player = true;
         int matches = 11;
         System.out.println("Количество спичек : " + matches);
         while (true) {
-            System.out.println("Очередь User1");
-            int user1 = Integer.valueOf(input.nextLine());
-            while (user1 > 3  || user1 < 1 || user1 > matches) {
-                System.out.println("Введите корректое значение");
-                 user1 = Integer.valueOf(input.nextLine());
-            }
-            matches = matches - user1;
-            System.out.println("User1 забрал " + user1 + " спичек. Осталось " + matches);
-            if (matches == 0) {
-                System.out.println("User1 победил!");
-                break;
-            }
-            System.out.println("Очередь User2");
-            int user2 = Integer.valueOf(input.nextLine());
-            while (user2 > 3  || user2 < 1 || user2 > matches) {
-                System.out.println("Введите корректое значение");
-                user2 = Integer.valueOf(input.nextLine());
-            }
-            matches = matches - user2;
-            System.out.println("User2 забрал " + user2 + " спичек. Осталось " + matches);
-            if (matches == 0) {
-                System.out.println("User2 победил!");
-                break;
-            }
+            String name = player ? "игрок1" : "игрок2";
+                System.out.println("Очередь " + name);
+                int count = Integer.valueOf(input.nextLine());
+                while (count > 3  || count < 1) {
+                    System.out.println("Введите корректое значение");
+                    count = Integer.valueOf(input.nextLine());
+                }
+                matches = matches - count;
+                System.out.println(name + " забрал " + count + " спичек. Осталось " + matches);
+                if (matches <= 0) {
+                    System.out.println(name + " победил!");
+                    break;
+                }
+               player = !player;
         }
     }
 }
