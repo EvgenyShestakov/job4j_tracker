2f3a8dc2a9a0b80a32082176f368bf6afdb36c0c
Shestakov Evgeny <lester_g@inbox.ru>
2020-08-13 15:02:53 +0300

    0.4. Пользовательские исключения. [#336384]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static void main(String[] args) {
+        try {
+            System.out.println(indexOf(new String[]{"Вася", "Толя", "Боря"}, "Саша"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw  new ElementNotFoundException("Index not found");
+        }
+        return rsl;
+    }
+}
