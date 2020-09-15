87e1342aa38d3f6febd3e69825b1842a2d54f541
Shestakov Evgeny <lester_g@inbox.ru>
2020-09-15 12:37:38 +0300

    4. Сортировка номера [#336336]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,12 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+     String[] one = left.split("\\.");
+     String[] two = right.split("\\.");
+     return Integer.compare(Integer.parseInt(one[0]), Integer.parseInt(two[0]));
+    }
+}
