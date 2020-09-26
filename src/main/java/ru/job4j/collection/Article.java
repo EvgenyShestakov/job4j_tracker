6bfa6ee7f276a3b9e6bbcd5295ea80fc88138db2
Shestakov Evgeny <lester_g@inbox.ru>
2020-09-26 11:26:19 +0300

    Collections. Lite. Exam.

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -1,18 +1,26 @@
 package ru.job4j.collection;
 
+import ru.job4j.tracker.StubOutput;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        String[] one = origin.split("[., :;!]");
-        String[] two = line.split("[., :;!]");
-        int count = 0;
-        for (String a: two) {
-            for (String b: one) {
-                if (a.equals(b)) {
-                    count++;
-                    break;
-                }
+        boolean flag = true;
+        String originMod = origin.replaceAll("\\p{Punct}", "");
+        String lineMod = line.replaceAll("\\p{Punct}", "");
+        String[] one = originMod.split("\\s");
+        String[] two = lineMod.split("\\s");
+        Set<String> set = new HashSet<>();
+        Collections.addAll(set, one);
+        for (String elem: two) {
+            if (!set.contains(elem)) {
+                flag = false;
+                break;
             }
         }
-        return count == two.length;
+        return flag;
     }
 }
