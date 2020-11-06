ad4ea3cdb35e23fdbd28716a09c38f52c69b2a22
Shestakov Evgeny <lester_g@inbox.ru>
2020-11-06 08:31:48 +0300

    EasyStream [#401629]

--- src/main/java/ru/job4j/stream/EasyStream.java
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -8,34 +8,46 @@ import java.util.function.Predicate;
 public class EasyStream {
     private List<Integer> list;
 
-    private EasyStream(List<Integer> list) {
-        this.list = list;
-    }
-
     public static EasyStream of(List<Integer> source) {
-      return new EasyStream(source);
+        return new StreamBuilder().withListInteger(source).build();
     }
 
     public EasyStream map(Function<Integer, Integer> fun) {
         List<Integer> tmp = new ArrayList<>();
         for (Integer integer : this.list) {
-           tmp.add(fun.apply(integer));
+            tmp.add(fun.apply(integer));
         }
-        return new EasyStream(tmp);
+        return new StreamBuilder().withListInteger(tmp).build();
     }
 
     public EasyStream filter(Predicate<Integer> fun) {
         List<Integer> tmp = new ArrayList<>();
         for (Integer integer : this.list) {
             if (fun.test(integer)) {
-             tmp.add(integer);
+                tmp.add(integer);
             }
         }
-        return new EasyStream(tmp);
+        return new StreamBuilder().withListInteger(tmp).build();
     }
 
     public List<Integer> collect() {
         return this.list;
     }
 
+    public static class StreamBuilder {
+        private EasyStream newStream;
+
+        public StreamBuilder() {
+            newStream = new EasyStream();
+        }
+
+        public StreamBuilder withListInteger(List<Integer> list) {
+            newStream.list = list;
+            return  this;
+        }
+
+        public EasyStream build() {
+            return  newStream;
+        }
+    }
 }
